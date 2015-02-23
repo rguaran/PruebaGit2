8e86b8eae56078cac91f1a1bac27b351b1637237
rguaran <ritaguaran@gmail.com>
2015-02-23 01:50:27 -0600

    cambios 2

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author RITA
+ */
+public class test {
+    public test(){
+    }
+    
+    int suma (int a , int b){
+    int res = a+b;
+    return res;
+    }
+}
